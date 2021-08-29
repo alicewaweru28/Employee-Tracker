860ae437e2320f104ba7e8ea94ed27c1f8fc4a83
Alice Waweru <80792502+alicewaweru28@users.noreply.github.com>
2021-08-29 10:28:39 -0700

    Delete db directory

--- db/seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO department(department_name)
-VALUES ("Marketing"), ("Sales"), ("Engineering"), ("Accounting"), ("Production");
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("IT Tech", 50000, 1), ("Marketing Assistant", 60000, 1), ("Manager", 130000, 3), ("Accountant", 80000, 4), ("Saled Lead", 5000, 5), ("Sales Manager", 75000, 5);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Will", "Smith", 1, null), ("Micheal", "Johnson", 1, 1), ("Jim", "Jones", 3, null), ("Nia", "Long", 5, 2), ("Frank", "Ocean", 5, 1);5
